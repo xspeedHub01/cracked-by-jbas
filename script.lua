54b836a86bfd0ed32ef6f6736734e3ea4c18a5f7
xspeedHub01 <GaelBlanco89@icloud.com>
2026-06-14 00:45:33 -0400

    Actualizar script.lua

--- script.lua
+++ script.lua
@@ -38,3 +38,44 @@ local Window = WindUI:CreateWindow({
 })
 Window:Tag({ Title = "v2.2", Color = Color3.fromHex("#ff3366"), Radius = 12 })
 Window:EditOpenButton({ Enabled = true })
+-- Pestaña de COMBATE
+local TabCombat = Window:Tab({
+    Title = "COMBAT",
+    Icon = "crosshair" -- Icono de mira
+})
+
+-- Pestaña de MOVIMIENTO
+local TabMovement = Window:Tab({
+    Title = "MOVEMENT",
+    Icon = "zap" -- Icono de rayo/velocidad
+})
+
+-- Pestaña de ARMAS
+local TabWeapon = Window:Tab({
+    Title = "WEAPON",
+    Icon = "sword" -- Icono de espada
+})
+
+-- Pestaña de VISUALES (ESP, etc)
+local TabVisual = Window:Tab({
+    Title = "VISUAL",
+    Icon = "eye" -- Icono de ojo
+})
+
+-- Pestaña de AUTO FARM
+local TabAutoFarm = Window:Tab({
+    Title = "AUTOFARM",
+    Icon = "repeat" -- Icono de repetición
+})
+
+-- Pestaña de MISCELÁNEOS (Otros)
+local TabMisc = Window:Tab({
+    Title = "MISC",
+    Icon = "plus-circle"
+})
+
+-- Pestaña de CONFIGURACIÓN
+local TabConfig = Window:Tab({
+    Title = "CONFIG",
+    Icon = "settings" -- Icono de tuerca
+})
